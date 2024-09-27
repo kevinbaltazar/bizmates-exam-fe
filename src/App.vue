320fac33d904eb782b2e3f004af815d07714aac5
kevinbaltazar <57998161+kevinbaltazar@users.noreply.github.com>
2024-09-27 09:59:14 +0800

    remove comment

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <script >
-import axios from 'axios'
-import { RouterLink, RouterView } from 'vue-router'
 import DashboardView from './views/DashboardView.vue'
 
 export default {
     components: { DashboardView }
-    // mounted() {
-    //     axios.get('http://127.0.0.1:8000/api/').then(function (res) {
-    //         console.log(res)
-    //     })
-    // }
 }
 </script>
 
@@ -17,15 +10,4 @@ export default {
   <div>
     <dashboard-view />
   </div>
-
-  <!-- <div class="wrapper">
-    <HelloWorld msg="You did it!" />
-
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <p class="text-black"></p>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </div> -->
-
 </template>
